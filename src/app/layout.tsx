660f280f3b12c2892a4b64bd92d16333c0d33d49
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2025-03-05 00:13:58 +0700

    fix(sign in): fix ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Provider } from "@/components/ui/provider";
-import '../../public/styles/fonts.css'
+import "../../public/styles/fonts.css";
 
 export const metadata: Metadata = {
   title: "Boost Your Dream",
@@ -14,10 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
+      <head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        />
+      </head>
       <body>
-        <Provider>
-          {children}
-        </Provider  >
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
--- src/components/signin/SignIn.tsx
+++ src/components/signin/SignIn.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import classNames from "classnames";
 import { useRouter } from "next/navigation";
 import classes from "./SignIn.module.css";
 import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
@@ -7,49 +9,124 @@ import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
 const SignIn = () => {
   const router = useRouter();
 
+  const [isSignUpClicked, setIsSignUpClicked] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUpClicked(!isSignUpClicked);
+  };
+
   return (
     <Flex className={classes.container}>
-    
-      <Box className={classes.box}>
-        <form className={classes.form}>
-        <Heading fontSize={'32px'} textAlign={'center'}>Login</Heading>
-          <div className={classes.inputBox}>
-            <input type="text" name="username" required />
-            <label>Username</label>
-            <i className="bx bxs-user"></i>
+      <div className={classes.wrapper}>
+        <span className={classNames(classes.wave, classes.animation, isSignUpClicked ? classes.active : "")}></span>
+        <span className={classNames(classes.wave2, classes.animation, isSignUpClicked ? classes.active : "")}></span>
+
+        <div className={classNames(classes.formBox, classes.login, isSignUpClicked ? classes.active : "")}>
+          <form className={classNames(classes.form, classes.animation)} action="#" >
+            <Heading fontSize={"32px"} textAlign={"center"} color={"white"}  >
+              Login
+            </Heading>
+            <div className={classes.inputBox}>
+              <input type="text" name="username" required />
+              <label>Username</label>
+              <i className="bx bxs-user"></i>
+            </div>
+
+            <div className={classes.inputBox}>
+              <input type="password" name="username" required />
+              <label>Password</label>
+              <i className="bx bxs-lock-alt"></i>
+            </div>
+
+            <Button
+              className={classes.btn}
+              type="submit"
+              onClick={() => router.push("/")}
+            >
+              Login
+            </Button>
+
+            <Box className={classes.link}>
+              <p>
+                Don't have an account ?{" "}
+                <a href="#" className={classNames(classes.btnSignup)} onClick={() => handleSignUpClick()}>
+                  Sign up
+                </a>
+              </p>
+            </Box>
+          </form>
+
+          <div className={classNames(classes.infoContent, classes.log, classes.animation)}>
+            <Heading
+              textTransform={"uppercase"}
+              fontSize={"36px"}
+              lineHeight={1.3}
+              color={"#fff"}
+            >
+              Welcome Back!
+            </Heading>
+            <p>You are welcome!</p>
           </div>
-
-          <div className={classes.inputBox}>
-            <input type="text" name="username" required />
-            <label>Password</label>
-            <i className="bx bxs-lock-alt"></i>
-          </div>
-
-          <Button
-            className={classes.btnLogin}
-            type="submit"
-            onClick={() => router.push("/")}
-          >
-            Login
-          </Button>
-
-          <Box className={classes.link}>
-            <p>
-              Don't have an account ?{" "}
-              <a href="#" className={classes.btnSignup}>
-                Sign Up
-              </a>
-            </p>
-          </Box>
-        </form>
-
-        <div className={classes.infoContent}>
-            <Heading textTransform={'uppercase'} fontSize={36} lineHeight={1.3}>Welcome Back!</Heading>
-            <p >Hello boy, lai la anh dayyyyyy</p>
         </div>
-      </Box>
+
+        <div className={classNames(classes.formBox, classes.register)}>
+        <form className={classNames(classes.form, classes.animation)} action="#" >
+            <Heading fontSize={"32px"} textAlign={"center"} color={"white"}>
+              Sign Up
+            </Heading>
+            <div className={classes.inputBox}>
+              <input type="text" name="username" required />
+              <label>Username</label>
+              <i className="bx bxs-user"></i>
+            </div>
+
+            <div className={classes.inputBox}>
+              <input type="password" name="password" required />
+              <label>Password</label>
+              <i className="bx bxs-lock-alt"></i>
+            </div>
+
+            <div className={classes.inputBox}>
+              <input type="password" name="repassword" required />
+              <label>Re-Password</label>
+              <i className="bx bxs-lock-alt"></i>
+            </div>
+
+            <Button
+              className={classes.btn}
+              type="submit"
+              onClick={() => router.push("/")}
+            >
+              Register
+            </Button>
+
+            <Box className={classes.link}>
+              <p>
+                Already have an account ?{" "}
+                <a href="#" className={classes.btnSignup}>
+                  Login
+                </a>
+              </p>
+            </Box>
+          </form>
+
+          <div className={classNames(classes.infoContent, classes.reg, classes.animation)}>
+            <Heading
+              textTransform={"uppercase"}
+              fontSize={"36px"}
+              lineHeight={1.3}
+              color={"#fff"}
+            >
+              Let's goooo
+            </Heading>
+            <p>You are welcome!</p>
+          </div>
+        </div>
+      </div>
     </Flex>
   );
 };
 
 export default SignIn;
+
+
